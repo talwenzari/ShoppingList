87c089d6f47b61d29b4eff56363241ab7641492d
Tuomas Talvensaari <tuomastalvensaari@gmail.com>
2012-01-09 16:17:30 +0200

    Basic functionality on main page

---
+++ assets/www/js/main.js
@@ -0,0 +1,35 @@
+$(function(){
+	$('#add-button').click(function() {
+		  console.log('Button clicked');
+		  
+		  var item = $('#new-item').val();
+		  if (item) {
+			  addItem(item);
+			  $('#new-item').val("");
+			  $('#list').listview('refresh');
+		  }
+	});
+
+	$('#open-file-button').click(function() {
+		// TODO Populate file list
+	});
+	
+	$('#list .delete-item-button').live('click', function() {
+		console.log("delete button clicked");
+		$(this).closest('li').remove();
+		$('#list').listview('refresh');
+	});
+	
+	$('#file-list .delete-item-button').live('click', function() {
+		// TODO confirm dialog + delete file
+	});
+	 document.addEventListener('deviceready', function () {
+		 console.log("PhoneGap is now loaded!");
+	 }, false);
+	 
+	 function addItem(item) {
+		  var newItem = $('#list-template li').clone();
+		  newItem.find('.item-text').html(item);
+		  $('#list').append(newItem);
+	 }
+});
--- assets/www/main.js
+++
@@ -1,5 +0,0 @@
-$('#page').live('pageinit',function(){
-	$('#add-button').click(function() {
-		$('#list').append($(document.createElement('li')).html($('#new-item').val()));
-	});
-}
