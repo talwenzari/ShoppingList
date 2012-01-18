9dc90f9ec497eff84fafd35c11ebec1e11dbc1e9
Tuomas Talvensaari <tuomastalvensaari@gmail.com>
2012-01-18 21:23:16 +0200

    Added "toggle checked" functionality

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -16,13 +16,18 @@ $(function(){
 	
 	$('#list .delete-item-button').live('click', function() {
 		console.log("delete button clicked");
-		$(this).closest('li').remove();
-		$('#list').listview('refresh');
+		$(this).closest('li').toggleClass('checked');
+		//$('#list').listview('refresh');
 	});
 	
 	$('#file-list .delete-item-button').live('click', function() {
 		// TODO confirm dialog + delete file
 	});
+	
+	$('#toggle-checked-button').click(function() {
+		$('.checked').toggle();
+	});
+	
 	 document.addEventListener('deviceready', function () {
 		 console.log("PhoneGap is now loaded!");
 	 }, false);
